af963538673b7010c26d4525d11f6eb94cfec144
Trevor Harmon <tharmon@laspositascollege.edu>
2023-10-16 17:55:44 -0700

    Add solution

--- main.cpp
+++ main.cpp
@@ -5,7 +5,14 @@
 
 using namespace std;
 
-int main()
+int main(int argc, char* argv[])
 {
+	if (argc != 2) {
+		cerr << "Expected a path argument" << endl;
+		return EXIT_FAILURE;
+	}
+
+    cout << getDirectorySize(argv[1]) << endl;
+
 	return EXIT_SUCCESS;
 }
--- src/file_space.cpp
+++ src/file_space.cpp
@@ -1,8 +1,20 @@
 #include "file_space.h"
+#include <filesystem>
 
 using namespace std;
+using namespace filesystem;
 
-int getDirectorySize(const string& path)
+int getDirectorySize(const std::string& path)
 {
-    return 0;
+    int size = 0;
+    directory_entry entry = directory_entry(path);
+    if (entry.is_directory()) {
+        for (const directory_entry& entry : directory_iterator(path)) {
+            size += getDirectorySize(entry.path());
+        }
+    } else {
+        size += entry.file_size();
+    }
+
+    return size;
 }
